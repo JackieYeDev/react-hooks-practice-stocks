3a32fb1b9098783dc5861ea7e64b12f4b48fba3b
Jackie <jye0325@gmail.com>
2022-07-11 19:52:05 -0400

    commit tree 14068b85885c993300ad8a65f35d6a6da3dde1fe

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+  const [stocks, setStocks] = useState([]);
+  const [portfolio, setPortfolio] = useState([]);
+  const [filter, setFilter] = useState("All");
+  const [sortCriteria, setSortCriteria] = useState("None");
+
+  useEffect(() => {
+    fetch("http://localhost:3001/stocks")
+      .then(res => res.json())
+      .then(data => setStocks(data));
+  }, []);
+
+  function handleClickBuy(stock) {
+    setPortfolio([...portfolio, stock]);
+  }
+
+  function handleClickSell(stock) {
+    setPortfolio(portfolio.filter(s => s.name !== stock.name));
+  }
+  function handleFilterChange(value) {
+    setFilter(value);
+  }
+  function handleSortChange(value) {
+    setSortCriteria(value);
+  }
+  const stocksToRender = stocks
+    .filter(stock => (filter === "All" ? true : stock.type === filter))
+    .sort((a, b) => {
+      if (sortCriteria === "None") {
+        return true;
+      } else if (sortCriteria === "Alphabetically") {
+        return a.name > b.name ? 1 : -1;
+      } else if (sortCriteria === "Price") {
+        return a.price > b.price ? 1 : -1;
+      }
+    });
+
   return (
     <div>
-      <SearchBar />
-      <div className="row">
-        <div className="col-8">
-          <StockContainer />
+      <SearchBar
+        handleFilterChange={handleFilterChange}
+        handleSortChange={handleSortChange}
+      />
+      <div className='row'>
+        <div className='col-8'>
+          <StockContainer
+            stocks={stocksToRender}
+            handleClick={handleClickBuy}
+          />
         </div>
-        <div className="col-4">
-          <PortfolioContainer />
+        <div className='col-4'>
+          <PortfolioContainer
+            portfolio={portfolio}
+            handleClick={handleClickSell}
+          />
         </div>
       </div>
     </div>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,36 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
-function SearchBar() {
+function SearchBar({ handleFilterChange, handleSortChange }) {
   return (
     <div>
       <strong>Sort by:</strong>
       <label>
         <input
-          type="radio"
-          value="Alphabetically"
-          name="sort"
+          type='radio'
+          value='Alphabetically'
+          name='sort'
           checked={null}
-          onChange={null}
+          onChange={e => handleSortChange(e.target.value)}
         />
         Alphabetically
       </label>
       <label>
         <input
-          type="radio"
-          value="Price"
-          name="sort"
+          type='radio'
+          value='Price'
+          name='sort'
           checked={null}
-          onChange={null}
+          onChange={e => handleSortChange(e.target.value)}
         />
         Price
       </label>
       <br />
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
-          <option value="Tech">Tech</option>
-          <option value="Sportswear">Sportswear</option>
-          <option value="Finance">Finance</option>
+        <select onChange={e => handleFilterChange(e.target.value)}>
+          <option value='Tech'>Tech</option>
+          <option value='Sportswear'>Sportswear</option>
+          <option value='Finance'>Finance</option>
         </select>
       </label>
     </div>
